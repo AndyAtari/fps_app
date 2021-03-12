660bdc71a9d370b260b00beac9bee69107ce2371
Andrew Alteri <andrewsalteri@gmail.com>
2021-03-12 12:18:14 -0600

    Added Consoles table for AR relationship with games.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_12_180415) do
+ActiveRecord::Schema.define(version: 2021_03_12_181656) do
+
+  create_table "consoles", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "games", force: :cascade do |t|
     t.string "title"
