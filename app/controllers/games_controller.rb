a0fce5a67c2383e5a5cceefab6afceac9169f461
Andrew Alteri <andrewsalteri@gmail.com>
2021-03-16 18:44:37 -0500

    added edit to before_action

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,6 @@
 class GamesController < ApplicationController
 
-    before_action :set_game, only: [:show]
+    before_action :set_game, only: [:show, :edit]
 
     def index
         @games = Game.all
