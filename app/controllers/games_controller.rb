f8829277283e94906bc2dfe934cfaa8fe055433c
Andrew Alteri <andrewsalteri@gmail.com>
2021-03-22 13:29:34 -0500

    Added a collection_select for consoles and updated games controller to redirect on successful game save.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -18,6 +18,9 @@ class GamesController < ApplicationController
 
     def create 
         @game = Game.new(game_params)
+        if @game.save
+            redirect_to game_path(@game)
+        end
     end
 
 
