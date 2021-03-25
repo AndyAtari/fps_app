2fa16b7600ca6c6c8e72ee1e1d2c7a22923598aa
Andrew Alteri <andrewsalteri@gmail.com>
2021-03-25 13:28:27 -0500

    Added Link column to Games table and added link on show page. Also added link section in new game form.

---
+++ db/migrate/20210325173612_add_link_to_games.rb
@@ -0,0 +1,5 @@
+class AddLinkToGames < ActiveRecord::Migration[6.0]
+  def change
+    add_column :games, :link, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_12_182744) do
+ActiveRecord::Schema.define(version: 2021_03_25_173612) do
 
   create_table "consoles", force: :cascade do |t|
     t.string "name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2021_03_12_182744) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "resolution"
     t.integer "console_id"
+    t.string "link"
   end
 
 end
